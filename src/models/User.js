9b881180e8d3b328b7285634584b19ce3688c5bc
Fabio <fabio.pengue@gmail.com>
2018-10-02 16:34:36 +0200

    User authentication
    User authentication on the server side completed and working

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
 const schema = new mongoose.Schema({
@@ -6,5 +8,25 @@ const schema = new mongoose.Schema({
   passwordHash: {type: String, required: true}
 }, {timestamps: true}
 );
+
+schema.methods.isValidPassword = function isValidPassword(password) {
+  return bcrypt.compareSync(password, this.passwordHash)
+};
+
+schema.methods.generateJWT = function generateJWT(){
+  return jwt.sign(
+    {
+    email: this.email
+    },
+    process.env.JWT_SECRET
+  );
+};
+
+schema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    email: this.email,
+    token: this.generateJWT()
+  }
+};
 
 export default mongoose.model('User', schema);
