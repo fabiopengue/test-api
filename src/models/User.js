085e00edd42ff362a82330e898e21125018b2d5e
Fabio <fabio.pengue@gmail.com>
2018-10-03 18:03:53 +0200

    User Signup
    User Sign Up completed, user recorded on the database

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 
 const schema = new mongoose.Schema({
-  email: { type: String, required: true, lowercase: true, index: true },
-  passwordHash: {type: String, required: true}
+  email: { type: String, required: true, lowercase: true, index: true, unique: true },
+  passwordHash: {type: String, required: true},
+  confirmed: {type: Boolean, default: false}
 }, {timestamps: true}
 );
 
@@ -13,10 +15,15 @@ schema.methods.isValidPassword = function isValidPassword(password) {
   return bcrypt.compareSync(password, this.passwordHash)
 };
 
+schema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = bcrypt.hashSync(password, 10)
+};
+
 schema.methods.generateJWT = function generateJWT(){
   return jwt.sign(
     {
-    email: this.email
+    email: this.email,
+    confirmed: this.confirmed
     },
     process.env.JWT_SECRET
   );
@@ -28,5 +35,7 @@ schema.methods.toAuthJSON = function toAuthJSON() {
     token: this.generateJWT()
   }
 };
+
+schema.plugin(uniqueValidator, {message: 'This email is already in use'})
 
 export default mongoose.model('User', schema);
